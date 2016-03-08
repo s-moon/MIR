4156da1616aaae30d001b2261c450fe116e1c6d9
Stephen Moon <stephen@logicalmoon.com>
2016-03-08 12:36:28 +0000

    Rounded corners for all buttons.

--- MIR/www/scripts/index.js
+++ MIR/www/scripts/index.js
@@ -23,6 +23,12 @@
         document.addEventListener( 'pause', onPause.bind( this ), false );
         document.addEventListener( 'resume', onResume.bind( this ), false );
         setDateAndTime();
+
+        document.getElementById('btnTakePhoto').onclick = function () {
+            navigator.camera.getPicture(function (imageUri) {
+                document.getElementById('inc-image').innerHTML = "<img src='" + imageUri + "'style='width: 75%;' />";
+            }, null, null);
+        }
         
         // TODO: Cordova has been loaded. Perform any initialization that requires Cordova here.
     };
