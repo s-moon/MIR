fda4c808175581a33d96962b3cfb71c5916dd730
Stephen Moon <stephen@logicalmoon.com>
2016-04-11 21:40:36 +0100

    Fixed Index action to automatically send the email.

--- MIRServer/MIRServer/Controllers/HomeController.cs
+++ MIRServer/MIRServer/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MIRServer.Infrastructure;
 using MIRServer.Models;
 
 namespace MIRServer.Controllers
@@ -16,8 +17,9 @@ namespace MIRServer.Controllers
         }
 
         [HttpPost]
-        public ViewResult Incident(Incident incident)
+        public ViewResult Index(Incident incident)
         {
+            Email.sendTheEmail();
             return View();
         }
     }
---
+++ MIRServer/MIRServer/Infrastructure/Email.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net.Mail;
+using System.Text;
+
+namespace MIRServer.Infrastructure
+{
+    public static class Email
+    {
+        public static void sendTheEmail()
+        {
+            SmtpClient client = new SmtpClient();
+            client.Port = 587;
+            client.Host = "auth.smtp.1and1.co.uk";
+            client.EnableSsl = true;
+            client.Timeout = 10000;
+            client.DeliveryMethod = SmtpDeliveryMethod.Network;
+            client.UseDefaultCredentials = false;
+            client.Credentials = new System.Net.NetworkCredential("test@moonsolutions.co.uk", "pwd");
+
+            MailMessage mm = new MailMessage("donotreply@mirserver.com", "stephen@moonsolutions.co.uk", "test", "test");
+            mm.BodyEncoding = UTF8Encoding.UTF8;
+            mm.DeliveryNotificationOptions = DeliveryNotificationOptions.OnFailure;
+
+            client.Send(mm);
+        }
+    }
+}
